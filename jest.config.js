75fb7f290d73d7818aa5e1fad06ae4bf499b7c17
iago soriano <iago.srm.is@gmail.com>
2021-10-03 18:17:16 -0300

    package.json refactored

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: "src",
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  setupFilesAfterEnv: [
+    "<rootDir>/__test__/setup.ts"
+  ],
+  setupFiles: [
+    "dotenv/config"
+  ],
+  moduleDirectories: [
+    "node_modules",
+    "src"
+  ],
+  moduleNameMapper: {
+    "@presentation": [
+      "<rootDir>/presentation/index.ts"
+    ],
+    "@locales": [
+      "<rootDir>/locales/index.ts"
+    ],
+    "@infrastructure": [
+      "<rootDir>/infrastructure/index.ts"
+    ],
+    "@application": [
+      "<rootDir>/application/index.ts"
+    ],
+    "@domain": [
+      "<rootDir>/domain/index.ts"
+    ]
+  }
+};
